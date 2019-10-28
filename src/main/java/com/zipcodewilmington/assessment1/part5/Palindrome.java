0bab36e7ac23108652106389b200fb0b1d7934d9
KaneKopetski <KaneKopetski@users.noreply.github.com>
2019-10-28 12:05:50 -0400

    Complete Palindrome and part5

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -1,8 +1,44 @@
 package com.zipcodewilmington.assessment1.part5;
 
+import com.zipcodewilmington.assessment1.part1.BasicStringUtils;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Palindrome {
 
-    public Integer countPalindromes(String input){
-        return null;
+    public String[] getAllSubStrings(String input) {
+        ArrayList<String> arr = new ArrayList();
+
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = i + 1; j <= input.length(); j++) {
+                arr.add(input.substring(i, j));
+            }
+
+        }
+        return arr.toArray(new String[0]);
+
     }
+
+
+    public Integer countPalindromes (String input){
+        String[] allSubStrings = getAllSubStrings(input);
+        Integer palindromeCounter = 0;
+
+        for (int i = 0; i < allSubStrings.length; i++) {
+            String tempString = BasicStringUtils.reverse(allSubStrings[i]);
+            if (allSubStrings[i].equals(tempString)) {
+                palindromeCounter++;
+            }
+        }
+
+        return palindromeCounter;
+    }
+
+
+
+
+
 }
+
+
